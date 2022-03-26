d88b4b7d23f3f21882b293d68bb5377c6924e396
tandiep48 <68583489+tandiep48@users.noreply.github.com>
2022-03-27 01:44:15 +0700

    27/3

--- app/src/main/java/com/example/taphoaapp/MainActivity.java
+++ app/src/main/java/com/example/taphoaapp/MainActivity.java
@@ -17,14 +17,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        try {
-            PackageInfo packageInfo = getPackageManager().getPackageInfo("android.androidpractice", PackageManager.GET_SIGNATURES);
-            for(Signature signature: packageInfo.signatures){
-                MessageDigest messageDigest = MessageDigest.getInstance("SHA");
-                messageDigest.update(signature.toByteArray());
-                Log.d("KeyHash", Base64.encodeToString(messageDigest.digest(),Base64.DEFAULT));
-            }
-        }catch (Exception e){
-        }
     }
 }
