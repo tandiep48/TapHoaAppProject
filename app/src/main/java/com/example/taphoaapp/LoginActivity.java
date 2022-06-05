f366c86b2ce698e80514ddff6ca6e0d1b5267d51
tandiep48 <68583489+tandiep48@users.noreply.github.com>
2022-06-05 09:42:38 +0700

    update (push data into Main)
    Xem các activity là Login, SignUp, User_EmailAnd_Pass

--- app/src/main/java/com/example/taphoaapp/LoginActivity.java
+++ app/src/main/java/com/example/taphoaapp/LoginActivity.java
@@ -13,9 +13,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.facebook.AccessToken;
-import com.facebook.CallbackManager;
-import com.facebook.login.LoginManager;
+//import com.facebook.AccessToken;
+//import com.facebook.CallbackManager;
+//import com.facebook.login.LoginManager;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -25,29 +25,34 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
-import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
+import java.net.URI;
+
+//import com.facebook.FacebookSdk;
+//import com.facebook.appevents.AppEventsLogger;
 
 import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
-    private static final int RC_SIGN_IN = 1000;
     private FirebaseAuth mAuth;
+    FirebaseUser firebaseUser;
     EditText email, pass;
     Button login, signup, forgot, Flogin, Glogin;
     GoogleSignInClient gsc;
+    GoogleSignIn gsi;
     GoogleSignInOptions gso;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mAuth = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
         gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
-        gsc = GoogleSignIn.getClient(this, gso);
+        gsc = GoogleSignIn.getClient(LoginActivity.this, gso);
         email = findViewById(R.id.TaiKhoan);
         pass = findViewById(R.id.MatKhau);
         login = findViewById(R.id.loginBtn);
@@ -68,11 +73,11 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 startActivity(new Intent(this, SignUpActivity.class));
                 break;
             case R.id.loginBtn:
-
                 LoginUser();
                 break;
             case R.id.LoginFBtn:
-                Intent intent = new Intent(LoginActivity.this, FacebookAuthActivity.class);
+                Intent intent = new Intent(LoginActivity.this,
+                        FacebookAuthActivity.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
                 startActivity(intent);
                 break;
@@ -113,52 +118,44 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             return;
         }
         mAuth.signInWithEmailAndPassword(Email, Password)
-                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                .addOnCompleteListener(LoginActivity.this,
+                        new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            if(user.isEmailVerified()){
-
-                                Intent intent = new Intent(LoginActivity.this, User_EmailAndPass_Activity.class);
-                                startActivity(intent);
-                            }else {
-                                user.sendEmailVerification();
-                                Toast.makeText(LoginActivity.this,"Xác nhận mail của bạn",Toast.LENGTH_LONG).show();
-                            }
+                            Intent intent = new Intent(LoginActivity.this,
+                                    User_EmailAndPass_Activity.class);
+                            startActivity(intent);
                         } else {
-                            Toast.makeText(LoginActivity.this, "Đăng nhập thất bại", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(LoginActivity.this, "Đăng nhập thất bại",
+                                    Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
     }
 
-    private void signIn() {
+    public void signIn() {
         Intent signInIntent = gsc.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);
+        startActivityForResult(signInIntent, 100);
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode,  Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == RC_SIGN_IN) {
+        if(requestCode == 100){
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-            if(account!=null){
-                navigateToUserActivity();
-            }
             try {
                 task.getResult(ApiException.class);
+                ChangeActivity();
             } catch (ApiException e) {
-                Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
             }
         }
     }
 
-    void navigateToUserActivity() {
+    private void ChangeActivity() {
         finish();
-        Intent intent = new Intent(LoginActivity.this, UserActivity.class);
+        Intent intent = new Intent(getApplicationContext(), User_EmailAndPass_Activity.class);
         startActivity(intent);
     }
-
 }
