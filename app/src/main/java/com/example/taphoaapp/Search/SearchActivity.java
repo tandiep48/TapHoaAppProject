4cb78525578d73669b79d44d758e196242dbd2a8
BaTung <baztung@gmail.com>
2022-06-23 07:30:00 +0700

    Tung Fix bug

--- app/src/main/java/com/example/taphoaapp/Search/SearchActivity.java
+++ app/src/main/java/com/example/taphoaapp/Search/SearchActivity.java
@@ -1,5 +1,7 @@
 package com.example.taphoaapp.Search;
 
+import static java.lang.Math.toIntExact;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -8,12 +10,20 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.SearchView;
 
 import com.example.taphoaapp.Basket.BasketProductAdapter;
+import com.example.taphoaapp.MainActivity;
 import com.example.taphoaapp.R;
 import com.example.taphoaapp.product_item;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.text.Collator;
 import java.util.ArrayList;
@@ -24,9 +34,10 @@ public class SearchActivity extends AppCompatActivity {
 
     private RecyclerView mRecycler;
     private search_adapter searchAdapter;
-      List<Search_Item>  ListOriginsearchAdapt;
+      List<Search_Item>  ListOriginsearchAdapt  = new ArrayList<>();;
     androidx.appcompat.widget.SearchView search;
     private Collator VNCollator;
+    private String name , category,tmp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,12 +47,15 @@ public class SearchActivity extends AppCompatActivity {
         VNCollator.setStrength(Collator.PRIMARY); //desired strength
 
         mRecycler = findViewById(R.id.searchView_recyclerview);
-        ListOriginsearchAdapt = getListProduct();
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.search_toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+         getListProduct();
+        searchAdapter = new search_adapter(ListOriginsearchAdapt,SearchActivity.this);
+
 
         search=findViewById(R.id.searchView);
 
@@ -53,18 +67,20 @@ public class SearchActivity extends AppCompatActivity {
             @Override
             public boolean onQueryTextSubmit(String query) {
 
-                return false;
+                return true;
             }
 
             @Override
             public boolean onQueryTextChange(String newText) {
-                searchword(newText);
-                return false;
+                searchAdapter.getFilter().filter(newText);
+                searchAdapter.notifyDataSetChanged();
+                return true;
             }
         });
 
 
-        searchAdapter = new search_adapter(ListOriginsearchAdapt,SearchActivity.this);
+
+
 
 //        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity(),RecyclerView.VERTICAL,false);
 //        mRecycler.setLayoutManager(linearLayoutManager);
@@ -76,49 +92,124 @@ public class SearchActivity extends AppCompatActivity {
         });
 
 
-        searchAdapter.addlist(ListOriginsearchAdapt);
-        mRecycler.setAdapter(searchAdapter);
     }
 
-    private void searchword(String textSearch){
-
-        ArrayList<Search_Item> searchList = new ArrayList<>();
-        if(textSearch.isEmpty()) searchAdapter.addlist(ListOriginsearchAdapt);;
-        if (searchList.size()>0){
-            searchAdapter.clear();
-        }
-        for ( Search_Item item:getListProduct()){
-
+//    private void searchword(String textSearch){
+//            List<Search_Item> searchList = new ArrayList<>();
+//
+//        for (Search_Item item : getListProduct()) {
+//            Log.e("Work",getListProduct().toString());
 //            if (item.getName().toLowerCase().contains(textSearch.toLowerCase())){
 //                searchList.add(item);
 //            }
-            if (VNcontains(item.getName().toLowerCase(),textSearch.toLowerCase()) == true){
-                searchList.add(item);
-            }
+//            if (VNcontains(item.getName().toLowerCase(), textSearch.toLowerCase())) {
+//                searchList.add(item);
+//            }
+//
+//        }
+//            if (textSearch.isEmpty() || textSearch.equalsIgnoreCase(" "))
+////                    || textSearch.trim().length() == 0)
+//            { searchAdapter.addlist(ListOriginsearchAdapt); }
+////        else if (searchList.size()>0){
+////            searchAdapter.clear();
+////        }
+//
+//            searchAdapter.addlist(searchList);
+//            searchAdapter.notifyDataSetChanged();
+//        }
 
-        }
-        searchAdapter.addlist(searchList);
-        searchAdapter.notifyDataSetChanged();
+
+
+    private void getListProduct(){
+        List<Search_Item> products = new ArrayList<>();
+//        products.add( new Search_Item("Áo thun nam POLO trơn vải cá sấu cotton cao cấp ngắn tay cực sang trọng","Quần áo"));
+//        products.add( new Search_Item("Mũ lưỡi trai ❤️ Nón kết thêu chữ Memorie phong cách Ulzzang","Quần áo"));
+//        products.add( new Search_Item("Quần áo phòng dịch đi máy bay đã kiểm định","Quần áo"));
+//        products.add( new Search_Item("Tất ngắn nam Vớ thấp cổ 4 màu trơn chống hôi chân","Quần áo"));
+//        products.add( new Search_Item("Bộ Quần Áo Mặc Nhà Thể Thao Nam Mùa Hè Phong Cách Cao Cấp ZERO","Quần áo"));
+//
+//        products.add( new Search_Item("Sách - Ngàn Năm Áo Mũ - Trần Quang Đức","Sách"));
+//        products.add( new Search_Item("Sách Chiếc Áo Xường Xám Màu Hoa Đào","Sách"));
+//        products.add( new Search_Item("Sách - Trường Phái Kinh Tế Học Áo - Lược Khảo","Sách"));
+//        products.add( new Search_Item("Bút gấu brow mặc áo cosplay ngộ nghĩnh ROSESHOP (C13)","văn phòng"));
+//
+//        products.add( new Search_Item("Pin cúc áo CR1632 Lithium 3V dùng cho các thiết bị điện tử","Điện tử"));
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+//
+        db.collection("SAN_PHAM")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                category = document.getString("CATEGORY");
+                                name = document.getString("NAME");
+
+
+                                products.add( new Search_Item(name,category));
+                                Log.e("documment", document.getId() + " => " + document.getData());
+                            }
+                            ListOriginsearchAdapt = products;
+                            searchAdapter.addlist(products);
+                            searchAdapter.notifyDataSetChanged();
+                            mRecycler.setAdapter(searchAdapter);
+
+//                            SortProduct = products;
+//                            proAdapter.setData(SortProduct);
+//                            mRecycler.setAdapter(proAdapter);
+//                            proAdapter.notifyDataSetChanged();
+//                            mSrlLayout.setRefreshing(false);
+
+                        } else {
+                            Log.e("documment", "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+
 
     }
 
-    private List<Search_Item> getListProduct(){
+
+
+    private void getListProductFileterd(String newtext){
         List<Search_Item> products = new ArrayList<>();
-        products.add( new Search_Item("Áo thun nam POLO trơn vải cá sấu cotton cao cấp ngắn tay cực sang trọng","Quần áo"));
-        products.add( new Search_Item("Mũ lưỡi trai ❤️ Nón kết thêu chữ Memorie phong cách Ulzzang","Quần áo"));
-        products.add( new Search_Item("Quần áo phòng dịch đi máy bay đã kiểm định","Quần áo"));
-        products.add( new Search_Item("Tất ngắn nam Vớ thấp cổ 4 màu trơn chống hôi chân","Quần áo"));
-        products.add( new Search_Item("Bộ Quần Áo Mặc Nhà Thể Thao Nam Mùa Hè Phong Cách Cao Cấp ZERO","Quần áo"));
+//
+//        products.add( new Search_Item("Pin cúc áo CR1632 Lithium 3V dùng cho các thiết bị điện tử","Điện tử"));
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+//
+        db.collection("SAN_PHAM")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                category = document.getString("CATEGORY");
+                                name = document.getString("NAME");
+                                if (name.toLowerCase().contains(newtext.toLowerCase())||VNcontains(name.toLowerCase(), newtext.toLowerCase())){
+                                    products.add( new Search_Item(name,category));
+                                }
 
-        products.add( new Search_Item("Sách - Ngàn Năm Áo Mũ - Trần Quang Đức","Sách"));
-        products.add( new Search_Item("Sách Chiếc Áo Xường Xám Màu Hoa Đào","Sách"));
-        products.add( new Search_Item("Sách - Trường Phái Kinh Tế Học Áo - Lược Khảo","Sách"));
-        products.add( new Search_Item("Bút gấu brow mặc áo cosplay ngộ nghĩnh ROSESHOP (C13)","văn phòng"));
+                                Log.e("documment", document.getId() + " => " + document.getData());
+                            }
+                            ListOriginsearchAdapt = products;
+                            searchAdapter.addlist(products);
+                            searchAdapter.notifyDataSetChanged();
+                            mRecycler.setAdapter(searchAdapter);
 
-        products.add( new Search_Item("Pin cúc áo CR1632 Lithium 3V dùng cho các thiết bị điện tử","Điện tử"));
+//                            SortProduct = products;
+//                            proAdapter.setData(SortProduct);
+//                            mRecycler.setAdapter(proAdapter);
+//                            proAdapter.notifyDataSetChanged();
+//                            mSrlLayout.setRefreshing(false);
 
+                        } else {
+                            Log.e("documment", "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
 
-        return products;
     }
 
     @Override
--- app/src/main/java/com/example/taphoaapp/Search/search_adapter.java
+++ app/src/main/java/com/example/taphoaapp/Search/search_adapter.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -14,20 +16,29 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.taphoaapp.DetailProduct.DetailProductActivity;
 import com.example.taphoaapp.R;
+import com.example.taphoaapp.product_item;
 import com.example.taphoaapp.profile.DonhangDetailActivity;
 
+import java.text.Collator;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
-public class search_adapter extends RecyclerView.Adapter<search_adapter.ViewHolder> {
+public class search_adapter extends RecyclerView.Adapter<search_adapter.ViewHolder> implements Filterable {
     private List<Search_Item> search_items;
+    private List<Search_Item> Org_search_items;
     private Context context;
+    private Collator VNCollator;
+
+
+
     public search_adapter(List<Search_Item> search_items, Context context) {
         this.search_items = search_items;
         this.context = context;
     }
     public void addlist(List<Search_Item> search_items){
         this.search_items=search_items;
+        this.Org_search_items = search_items;
         notifyDataSetChanged();
     }
     @NonNull
@@ -77,5 +88,57 @@ public class search_adapter extends RecyclerView.Adapter<search_adapter.ViewHold
             category =itemView.findViewById(R.id.tv_search_category);
             linearLayout = itemView.findViewById(R.id.search);
         }
+    }
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                String strSearch =charSequence.toString();
+                if(strSearch.isEmpty()) search_items = Org_search_items;
+                else{
+                    List<Search_Item> list = new ArrayList<>();
+                    for(Search_Item product : Org_search_items){
+                        if(product.getName().toLowerCase().contains(strSearch.toLowerCase())|| VNcontains(product.getName().toLowerCase(), strSearch.toLowerCase())){
+                            list.add(product);
+                        }
+                    }
+                    search_items = list;
+                }
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = search_items;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                search_items = (List<Search_Item>) filterResults.values;
+                notifyDataSetChanged();
+            }
+        };
+    }
+
+    private boolean VNcontains(String source, String target) {
+        VNCollator = Collator.getInstance(new Locale("vi", "VN")); //Your locale here
+        VNCollator.setStrength(Collator.PRIMARY); //desired strength
+        if (target.length() > source.length()) {
+            return false;
+        }
+
+        Collator collator = Collator.getInstance();
+        collator.setStrength(Collator.PRIMARY);
+
+        int end = source.length() - target.length() + 1;
+
+        for (int i = 0; i < end; i++) {
+            String sourceSubstring = source.substring(i, i + target.length());
+
+            if (collator.compare(sourceSubstring, target) == 0) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
